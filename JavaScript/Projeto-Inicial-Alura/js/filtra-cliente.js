f0dbe50a9c3c6f705024bfccb138229efea84fc2
Filipe FPS <filipedepaula77@gmail.com>
2019-07-19 17:13:31 -0300

    Adição da função do filtro de clientes por nome

---
+++ JavaScript/Projeto-Inicial-Alura/js/filtra-cliente.js
@@ -0,0 +1,26 @@
+var campoFiltro = document.querySelector("#campo-busca");
+
+campoFiltro.addEventListener("input", function () {
+
+    var pacientes = document.querySelectorAll(".paciente");
+
+    if (this.value.length > 0) {
+        pacientes.forEach(function (paciente) {
+
+            var nome = paciente.querySelector(".info-nome").textContent;
+
+            var busca = new RegExp(campoFiltro.value, "i");
+
+            if (!(busca.test(nome))) {
+                paciente.classList.add("invisivel");
+            } else {
+                paciente.classList.remove("invisivel");
+            }
+        });
+
+    } else {
+        pacientes.forEach(function (paciente) {
+            paciente.classList.remove("invisivel");
+        });
+    }
+});
