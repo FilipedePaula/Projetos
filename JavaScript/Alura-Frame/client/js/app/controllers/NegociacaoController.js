883a02aee0a9e9e020eea4e2d8db6f3d8c3ba1ba
Filipe FPS <filipedepaula77@gmail.com>
2019-09-06 21:39:32 -0300

    Criação do controller. Uso de spread, map e arrow function.

--- JavaScript/Alura-Frame/client/js/app/controllers/NegociacaoController.js
+++ JavaScript/Alura-Frame/client/js/app/controllers/NegociacaoController.js
@@ -1,7 +1,51 @@
 class NegociacaoController {
 
+    constructor() {
+
+        let $ = document.querySelector.bind(document);
+
+        this._data = $('#data');
+        this._quantidade = $('#quantidade');
+        this._valor = $('#valor');
+
+    }
+
     adiciona(event) {
         event.preventDefault();
-        alert('Deu certo!');
+
+        let data = new Date(...
+            this._data.value.split('-')
+            .map(function (item, indice) {
+                return item - indice % 2;
+            })
+        );
+
+        console.log(data);
+
+        let dataFormatada = data.getDate() + '/' +
+            (data.getMonth() + 1) + '/' + data.getFullYear();
+
+        console.log(dataFormatada);
+
+        let negociacao = new Negociacao(
+            data,
+            this._quantidade.value,
+            this._valor.value
+        );
+
+        console.log(negociacao);
+
+        this.reinicia();
+    }
+
+    reinicia() {
+
+        let $ = document.querySelector.bind(document);
+
+        $('#data').value = '';
+        $('#quantidade').value = 0;
+        $('#valor').value = 0;
+
+        $('#data').focus();
     }
 }
